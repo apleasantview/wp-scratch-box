05462341a403e7cd3e55e8605c9e9d5562178851
Cristovao Verstraeten <cristovao@apleasantview.com>
2015-11-30 17:31:48 +0100

    Pre-release 2.0.2-alpha

--- wp-scratch-box.sh
+++ wp-scratch-box.sh
@@ -3,7 +3,7 @@
 #description    :provisioning script for wp-scratch-box
 #author         :Cristovao Verstraeten
 #date           :20151120
-#version        :2.0.0-alpha
+#version        :2.0.2-alpha
 #usage          :vagrant up --provision, vagrant provision
 #notes          :
 #bash_version   :4.3.39(3)-release

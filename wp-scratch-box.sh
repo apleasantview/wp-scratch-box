ca97536d63e8a789d45529671813411422722a67
Cristovao Verstraeten <cristovao@apleasantview.com>
2015-12-01 18:46:46 +0100

    Set empty array for custom WP parameters

--- wp-scratch-box.sh
+++ wp-scratch-box.sh
@@ -3,7 +3,7 @@
 #description    :provisioning script for wp-scratch-box
 #author         :Cristovao Verstraeten
 #date           :20151120
-#version        :2.1.0-alpha
+#version        :2.1.1-alpha
 #usage          :vagrant up --provision, vagrant provision
 #notes          :
 #bash_version   :4.3.39(3)-release
@@ -17,6 +17,7 @@ main() {
   additional_repos
   sudo apt-get update
   lamp_install
+  wp_custom=()
   wordpress
   reset_directory_permissions
   # set +x

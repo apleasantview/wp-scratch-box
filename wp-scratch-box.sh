cd384ed64101be2d88ff9200cfaa48094365d9dd
Cristovao Verstraeten <cristovao@apleasantview.com>
2015-12-01 18:53:14 +0100

    Use JQ to read custom WP parameters from Vagrant.json

--- wp-scratch-box.sh
+++ wp-scratch-box.sh
@@ -3,7 +3,7 @@
 #description    :provisioning script for wp-scratch-box
 #author         :Cristovao Verstraeten
 #date           :20151120
-#version        :2.1.1-alpha
+#version        :2.1.3-alpha
 #usage          :vagrant up --provision, vagrant provision
 #notes          :
 #bash_version   :4.3.39(3)-release
@@ -17,7 +17,7 @@ main() {
   additional_repos
   sudo apt-get update
   lamp_install
-  wp_custom=()
+  wp_custom=($(jq -r 'if .Project.wordpress then .Project.wordpress|.[] else empty end' /vagrant/Vagrant.json))
   wordpress
   reset_directory_permissions
   # set +x

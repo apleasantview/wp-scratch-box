bf8db7a6d220e3edc981f7fbcf2cf94cf898df9d
Cristovao Verstraeten <cristovao@apleasantview.com>
2015-12-17 14:44:02 +0100

    Include php5 module in Apache configurations

--- wp-scratch-box.sh
+++ wp-scratch-box.sh
@@ -71,7 +71,8 @@ apache_install() {
 
 apache_configurations() {
   sudo apt-get install -y \
-    libapache2-mod-auth-mysql
+    libapache2-mod-auth-mysql \
+    libapache2-mod-php5
   sudo a2enmod proxy proxy_fcgi rewrite
   sudo service apache2 restart
 

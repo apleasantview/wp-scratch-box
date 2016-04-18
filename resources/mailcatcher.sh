c5258f53b8c6bb0757f8814014b094e95f72b2b9
Cristovao Verstraeten <cristovao@apleasantview.com>
2016-04-18 09:10:09 +0200

    Update script to automatically get the latest version, add upstart configuration

--- resources/mailcatcher.sh
+++ resources/mailcatcher.sh
@@ -1,23 +1,100 @@
-#!/bin/bash -   
+#!/bin/bash -
 #title          :mailcatcher.sh
 #description    :Installs MailCatcher on wp-scratch-box VM
 #author         :Cristovao Verstraeten
 #date           :20151202
-#version        :20151202
+#version        :20160417
 #usage          :bash mailcatcher.sh
-#notes          :       
+#notes          :
 #bash_version   :4.3.39(3)-release
 #============================================================================
 set -euo pipefail
 IFS=$'\n\t'
 
 main(){
-    sudo apt-get install -y build-essential ruby1.9.1-dev libsqlite3-dev
-    sudo gem install --no-rdoc --no-ri mailcatcher
-    printf "sendmail_path = /var/lib/gems/1.9.1/gems/mailcatcher-0.6.1/bin/catchmail\n" | sudo tee /etc/php5/mods-available/mailcatcher.ini
-    sudo php5enmod mailcatcher
-    sudo service php5-fpm restart; sudo service apache2 restart
-    mailcatcher --ip=0.0.0.0 -v
+  # set -x
+  required_packages
+  rbenv_ruby_install
+  mailcatcher_install
+  # set +x
+}
+
+required_packages(){
+  sudo apt-get purge -y \
+    ruby
+  sudo apt-get update && sudo apt-get install -y \
+    autoconf \
+    bison \
+    build-essential \
+    libffi-dev \
+    libgdbm-dev \
+    libgdbm3 \
+    libncurses5-dev \
+    libreadline6-dev \
+    libsqlite3-dev \
+    libssl-dev \
+    libyaml-dev \
+    openssl \
+    zlib1g-dev
+}
+
+rbenv_ruby_install(){
+  local ruby_version="2.3.0"
+  
+  git clone git://github.com/sstephenson/rbenv.git /home/vagrant/.rbenv
+  git clone git://github.com/sstephenson/ruby-build.git /home/vagrant/.rbenv/plugins/ruby-build
+  
+  (
+    cd /home/vagrant/.rbenv
+    src/configure
+    make -C src
+  )
+  
+  {
+    echo "export PATH=$HOME/.rbenv/bin:$PATH";
+    echo 'eval "$(rbenv init -)"'
+  } >> /home/vagrant/.bash_profile
+  source /home/vagrant/.bash_profile
+
+  RUBY_CONFIGURE_OPTS=--disable-install-doc rbenv install "$ruby_version"
+  rbenv global "$ruby_version"
+  rbenv rehash
+}
+
+mailcatcher_install(){
+  gem install mailcatcher --no-document
+  rbenv rehash
+  mailcatcher_php
+  mailcatcher_upstart
+  
+  mailcatcher --ip=0.0.0.0 -v
+}
+
+mailcatcher_php(){
+  local mailcatcher_path
+  mailcatcher_path=$(rbenv which catchmail)
+
+  printf "sendmail_path = %s\n" "$mailcatcher_path" | sudo tee /etc/php5/mods-available/mailcatcher.ini
+  sudo php5enmod mailcatcher
+  sudo service php5-fpm restart; sudo service apache2 restart
+}
+
+mailcatcher_upstart(){
+  local mailcatcher_path
+  local ruby_path
+  mailcatcher_path=$(rbenv which mailcatcher)
+  ruby_path=$(rbenv which ruby)
+
+  cat << EOF | sudo tee /etc/init/mailcatcher.conf
+description "Mailcatcher"
+
+start on runlevel [2345]
+stop on runlevel [!2345]
+
+respawn
+
+exec $ruby_path $mailcatcher_path --foreground --http-ip=0.0.0.0
+EOF
 }
 
 main

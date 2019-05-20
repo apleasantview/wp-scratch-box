c5bca0877abedb94d4802aaf7bff7f7c2852b94e
Cristovao Verstraeten <cristovao@apleasantview.com>
2019-05-20 21:50:04 +0200

    Add suggested php packages for wp

--- wp-scratch-box.sh
+++ wp-scratch-box.sh
@@ -103,9 +103,9 @@ EOF
 
 phpfpm_install() {
   sudo apt-get install -y php7.2-fpm \
-    php7.2-cli php7.2-common php7.2-curl \
-    php7.2-gd php7.2-imap php7.2-json php7.2-mbstring php7.2-mysql php7.2-xml \
-    php7.2-xmlrpc php7.2-zip php-imagick php-pear
+    php7.2-cli php7.2-common php7.2-bcmath php7.2-curl \
+    php7.2-gd php7.2-imap php7.2-json php7.2-mbstring php7.2-mysql php7.2-soap \
+    php7.2-xml php7.2-xmlrpc php7.2-zip php-imagick php-pear
   sudo cp /vagrant/resources/custom-php.ini /etc/php/7.2/mods-available/
   sudo phpenmod custom-php
   

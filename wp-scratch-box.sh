5b472fa912d01b6158169014382f4f6c2757bfdf
Cristovao Verstraeten <cristovao@apleasantview.com>
2021-04-06 11:59:23 +0200

    fix(Provisioning): Imagick lib needed PHP version

--- wp-scratch-box.sh
+++ wp-scratch-box.sh
@@ -109,7 +109,7 @@ phpfpm_install() {
   sudo apt-get install -y php7.4-fpm \
     php7.4-cli php7.4-common php7.4-bcmath php7.4-curl \
     php7.4-gd php7.4-imap php7.4-json php7.4-mbstring php7.4-mysql php7.4-soap \
-    php7.4-xml php7.4-xmlrpc php7.4-zip php-imagick php-pear
+    php7.4-xml php7.4-xmlrpc php7.4-zip php7.4-imagick php-pear
   sudo cp /vagrant/resources/custom-php.ini /etc/php/7.4/mods-available/
   sudo phpenmod custom-php
   

5e641d381a992b30e8523c41f8298383c1b8ccc1
Cristovao Verstraeten <cristovao@apleasantview.com>
2023-11-02 08:33:03 +0100

    fix: One too many -f

--- wp-scratch-box.sh
+++ wp-scratch-box.sh
@@ -124,7 +124,7 @@ phpfpm_install() {
   sudo cp -f /vagrant/resources/php/custom-php.ini /etc/php/${PHPVERSION}/fpm/conf.d/php-custom.ini
   sudo cp -f /vagrant/resources/php/opcache.ini /etc/php/${PHPVERSION}/fpm/conf.d/opcache.ini
   sudo cp -f /vagrant/resources/php/mailpit.ini /etc/php/8.1/mods-available/mailpit.ini
-  sudo cp -f -f /vagrant/resources/php/xdebug.ini /etc/php/${PHPVERSION}/mods-available/xdebug.ini
+  sudo cp -f /vagrant/resources/php/xdebug.ini /etc/php/${PHPVERSION}/mods-available/xdebug.ini
 
   # sudo phpenmod custom-php
   sudo phpenmod mailpit
